ab1ec20394a7a84cda2e2cb78133a94da08d337c
Robert <Robert.pinney@ecit.no>
2024-03-04 17:07:36 +0100

    not perfect but exercise complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
-function App() {
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+import { useEffect, useState } from "react";
+
+export default function App() {
+	const [amount, setAmount] = useState(1);
+	const [currencyOne, setCurrencyOne] = useState("USD");
+	const [currencyTwo, setCurrencyTwo] = useState("EUR");
+	const [converted, setConverted] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(
+		function () {
+			async function convert() {
+				setIsLoading(true);
+				const res = await fetch(
+					`https://api.frankfurter.app/latest?amount=${amount}&from=${currencyOne}&to=${currencyTwo}`
+				);
+				const data = await res.json();
+				console.log(data);
+				setConverted(data.rates[currencyTwo]);
+				setIsLoading(false);
+			}
+
+			if (currencyOne === setCurrencyTwo) return setConverted(amount);
+			convert();
+		},
+		[amount, currencyOne, currencyTwo]
+	);
+
 	return (
-		<div className="App">
-			<p>Hello</p>
+		<div>
+			<form>
+				<input
+					type="text"
+					value={amount}
+					onChange={(e) => setAmount(Number(e.target.value))}
+					placeholder="1"
+					disabled={isLoading}
+				/>
+				<select
+					value={currencyOne}
+					onChange={(e) => setCurrencyOne(e.target.value)}
+					disabled={isLoading}
+				>
+					<option value="USD" selected>
+						USD
+					</option>
+					<option value="EUR">EUR</option>
+					<option value="CAD">CAD</option>
+					<option value="INR">INR</option>
+				</select>
+				<select
+					value={currencyTwo}
+					onChange={(e) => setCurrencyTwo(e.target.value)}
+					disabled={isLoading}
+				>
+					<option value="USD">USD</option>
+					<option value="EUR" selected>
+						EUR
+					</option>
+					<option value="CAD">CAD</option>
+					<option value="INR">INR</option>
+				</select>
+				<p>{`${amount} ${currencyOne} is equal to ${converted} ${currencyTwo}.`}</p>
+			</form>
 		</div>
 	);
 }
-
-export default App;
